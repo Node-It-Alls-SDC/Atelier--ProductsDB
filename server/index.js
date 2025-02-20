e6bf7ae9a6dcb108f5c112362209e34c55f22b77
Byron Work <workbyron22@gmail.com>
2025-02-20 17:18:35 -0500

    Added clustering for server

--- server/index.js
+++ server/index.js
@@ -1,34 +1,50 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const cluster = require('cluster');
+const os = require('os');
 // const { handleETL } = require('../utils/handleETL.js');
 const { getProducts, getStyles, getProductList, getRelated } = require('./routes.js');
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, './public')));
+if (cluster.isMaster) {
+  const cpuCount = os.cpus().length;
 
-mongoose.connect('mongodb://3.135.221.100:27017/products')
-  .then(() => console.log('connected to products DB'))
-  .catch(() => console.error('Failed to connect to DB'))
+  for (let i = 0; i < cpuCount; i++) {
+    console.log('forking...');
+    cluster.fork();
+  }
+  cluster.on('exit', (worker, code, signal) => {
+    cluster.fork();
+  });
+} else {
+  app.use(express.json());
+  app.use(express.static(path.join(__dirname, './public')));
 
 
-// LOADER.IO VERIFICATION
-app.get('/loaderio-be33675596a288215fca02c84d2d56d8.txt', (req, res) => {
-  res.sendFile(path.join(__dirname, './public', 'loaderio-be33675596a288215fca02c84d2d56d8.txt'));
-});
+  mongoose.connect('mongodb://3.135.221.100:27017/products')
+    .then(() => console.log('connected to products DB'))
+    .catch(() => console.error('Failed to connect to DB'))
 
-// ROUTES
-app.get('/products', getProductList);
-app.get('/products/:product_id', getProducts);
-app.get('/products/:product_id/styles', getStyles);
-app.get('/products/:product_id/related', getRelated);
 
-app.listen(PORT, () => {
-  console.log(`currently listening on port ${PORT}`);
-});
+  // LOADER.IO VERIFICATION
+  app.get('/loaderio-be33675596a288215fca02c84d2d56d8.txt', (req, res) => {
+    res.sendFile(path.join(__dirname, './public', 'loaderio-be33675596a288215fca02c84d2d56d8.txt'));
+  });
 
-module.exports = { app };
+  // ROUTES
+  app.get('/products', getProductList);
+  app.get('/products/:product_id', getProducts);
+  app.get('/products/:product_id/styles', getStyles);
+  app.get('/products/:product_id/related', getRelated);
+
+  app.listen(PORT, () => {
+    console.log(`currently listening on port ${PORT}`);
+  });
+
+  module.exports = { app };
+}
+
 
