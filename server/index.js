91ce8d1aa99ef3dd170695ec3487378668e03a89
Byron Work <workbyron22@gmail.com>
2025-02-20 19:05:01 -0500

    Removed loaderIO verification route, nginx instance covers it

--- server/index.js
+++ server/index.js
@@ -8,18 +8,12 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, './public')));
 
 
 mongoose.connect('mongodb://3.135.221.100:27017/products')
   .then(() => console.log('connected to products DB'))
   .catch(() => console.error('Failed to connect to DB'))
 
-
-// LOADER.IO VERIFICATION
-app.get('/loaderio-be33675596a288215fca02c84d2d56d8.txt', (req, res) => {
-  res.sendFile(path.join(__dirname, './public', 'loaderio-be33675596a288215fca02c84d2d56d8.txt'));
-});
 
 // ROUTES
 app.get('/products', getProductList);
